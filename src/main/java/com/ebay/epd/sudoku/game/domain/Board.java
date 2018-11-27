2e821b3704a93e63fc171f448592e5fac15bfdfa
David Palomar <david.palomar@servexternos.gruposantander.es>
2018-11-27 17:29:35 +0100

    add lombok dependency

--- src/main/java/com/ebay/epd/sudoku/game/domain/Board.java
+++ src/main/java/com/ebay/epd/sudoku/game/domain/Board.java
@@ -1,9 +1,13 @@
 package com.ebay.epd.sudoku.game.domain;
 
 import com.ebay.epd.sudoku.game.service.BoardState;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
+@NoArgsConstructor
+@Data
 public class Board {
 
     @NotNull
@@ -16,35 +20,5 @@ public class Board {
     public String dealsLink;
     private BoardState state = BoardState.VALID;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Integer[][] getFields() {
-        return fields;
-    }
-
-    public void setFields(Integer[][] fields) {
-        this.fields = fields;
-    }
-
-    public void setState(BoardState state) {
-        this.state = state;
-    }
-
-    public BoardState getState() {
-        return state;
-    }
-
-    public String getDealsLink() {
-        return dealsLink;
-    }
-
-    public void setDealsLink(String dealsLink) {
-        this.dealsLink = dealsLink;
-    }
 }
